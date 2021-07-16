1b3d1607d764d5083581cef0d33a40a75c656956
jjperezaguinaga <me@jjperezaguinaga.com>
2021-07-16 16:52:57 +0000

    chore: Refactored deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,15 @@
-require('dotenv').config()
-import { HardhatUserConfig, task} from 'hardhat/config'
+require("dotenv").config();
+import { HardhatUserConfig, task } from "hardhat/config";
 // import { HardhatUserConfig, task, types } from 'hardhat/config'
 // import 'hardhat-typechain'
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-waffle'
-import '@nomiclabs/hardhat-solhint'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-solhint'
-import 'solidity-coverage'
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-solhint";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-deploy";
+import "solidity-coverage";
+const { DEPLOYER_PRIVATE_KEY, ADMIN_ACCOUNT, ALICE, QUIKNODE_KEY } = process.env;
 
 // // This is a sample Hardhat task. To learn how to create your own go to
 // // https://hardhat.org/guides/create-task.html
@@ -19,46 +21,54 @@ import 'solidity-coverage'
 //   }
 // });
 
-const {ETHERSCAN} = process.env
+const { ETHERSCAN } = process.env;
 
 const hardhatConfig: HardhatUserConfig = {
-  defaultNetwork: 'localhost',
+  defaultNetwork: "localhost",
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
       chainId: 1337,
     },
+    xdai: {
+      chainId: 100,
+      url: `https://still-patient-forest.xdai.quiknode.pro/${QUIKNODE_KEY}/`,
+      accounts: DEPLOYER_PRIVATE_KEY
+        ? [DEPLOYER_PRIVATE_KEY, ADMIN_ACCOUNT, ALICE]
+        : [],
+    },
   },
   solidity: {
     compilers: [
       {
-        version: '0.8.6',
+        version: "0.8.6",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
-      }
-    ]
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   paths: {
-    sources: './contracts',
-    tests: './test',
-    cache: './hardhat/cache',
-    artifacts: './hardhat/artifacts'
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./hardhat/cache",
+    artifacts: "./hardhat/artifacts",
+    deployments: "./deployments",
   },
   typechain: {
-    outDir: './lib/types',
-    target: 'ethers-v5'
+    outDir: "./lib/types",
+    target: "ethers-v5",
   },
   etherscan: {
-    apiKey: ETHERSCAN
-  }
-}
+    apiKey: ETHERSCAN,
+  },
+};
 
-task('extract', 'Extract ABIs to specified folder', async (...args: any[]) => {
-  return (await import('./tasks/extract')).default(args[0], args[1], args[2])
-}).addFlag('target', 'Folder to output contents to')
+task("extract", "Extract ABIs to specified folder", async (...args: any[]) => {
+  return (await import("./tasks/extract")).default(args[0], args[1], args[2]);
+}).addFlag("target", "Folder to output contents to");
 
-export default hardhatConfig
+export default hardhatConfig;
